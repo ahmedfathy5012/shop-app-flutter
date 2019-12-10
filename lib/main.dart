7dceb5c5f675c27596558f4a0c3689fdf760c085
ahmedfathy5012 <fathy5012@gmail.com>
2019-12-09 22:51:59 -0800

    add products provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/products.dart';
+import 'package:provider/provider.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato' ,
+    return ChangeNotifierProvider(
+      builder: (context)=>Products(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'My Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato' ,
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+            ProductDetailScreen.routeName: (context)=> ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-          ProductDetailScreen.routeName: (context)=> ProductDetailScreen(),
-      },
     );
   }
 }
